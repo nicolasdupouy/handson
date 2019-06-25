b828b63e4b9a1ffc64fa8439403b5a4958a06684
Nicolas Dupouy <nicolasdupouy@gmail.com>
2019-06-25 18:22:00 +0200

    Remove the local user-activity repository for the benefit of the user-activity microservice

--- monolith/src/main/java/com/homics/monolith/service/UserActivityService.java
+++ monolith/src/main/java/com/homics/monolith/service/UserActivityService.java
@@ -2,72 +2,30 @@ package com.homics.monolith.service;
 
 import com.homics.monolith.controller.dto.UserActivityDto;
 import com.homics.monolith.model.ActivityType;
-import com.homics.monolith.model.UserActivity;
-import com.homics.monolith.repository.UserActivityRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Instant;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class UserActivityService {
-    // TODO 1.2.3:
-    //  Clean the code in monolith
-    //    - remove userActivityRepository
-    //    - remove the getUserActivities() and the mapActivity
-
-    private UserActivityRepository userActivityRepository;
-
     private RestTemplate restTemplate;
 
-    public UserActivityService(UserActivityRepository userActivityRepository, RestTemplate restTemplate) {
-        this.userActivityRepository = userActivityRepository;
+    public UserActivityService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     public void addLogin(String userName) {
-        // TODO 1.2.2:
-        //  Change UserActivity into UserActivityDto
-        UserActivity userActivity = new UserActivity();
-        userActivity.setUsername(userName);
-        userActivity.setActivityDate(Instant.now());
-        userActivity.setActivityType(ActivityType.LOGIN);
-        // TODO 1.2.2:
-        //  Remove save and use the post method that you just implemented
-        userActivityRepository.save(userActivity);
+        UserActivityDto userActivityDto = new UserActivityDto(userName, ActivityType.LOGIN, Instant.now());
+        post(userActivityDto);
     }
 
     public void addLogout(String userName) {
-        // TODO 1.2.2:
-        //  Change UserActivity into UserActivityDto
-        UserActivity userActivity = new UserActivity();
-        userActivity.setUsername(userName);
-        userActivity.setActivityDate(Instant.now());
-        userActivity.setActivityType(ActivityType.LOGOUT);
-        // TODO 1.2.2:
-        //  Remove save and use the post method that you just implemented
-        userActivityRepository.save(userActivity);
+        UserActivityDto userActivityDto = new UserActivityDto(userName, ActivityType.LOGOUT, Instant.now());
+        post(userActivityDto);
     }
 
-    public void post(UserActivityDto userActivityDto) {
-        // TODO 1.2.1:
-        //  Change the function below to call the right url with the right object
-        //  Remember that the url is on localhost:9001 and you used it to add an activity
-        //  The object you are sending is a UserActivityDto
-        restTemplate.postForEntity("MY_URL", "MY_OBJECT", Void.class);
+    private void post(UserActivityDto userActivityDto) {
+        restTemplate.postForEntity("http://localhost:9001/user/api/activity", userActivityDto, Void.class);
     }
-
-    public List<UserActivityDto> getUserActivities() {
-        return userActivityRepository.findAll()
-                .stream()
-                .map(this::mapActivity)
-                .collect(Collectors.toList());
-    }
-
-    private UserActivityDto mapActivity(UserActivity userActivity) {
-        return new UserActivityDto(userActivity.getUsername(), userActivity.getActivityType(), userActivity.getActivityDate());
-    }
-
 }
